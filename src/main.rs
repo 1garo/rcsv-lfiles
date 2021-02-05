74d5fc3de03cb7b5461eabacd1aacfb3bcc88a1e
1garo <alevardai@hotmail.com>
2021-02-05 20:39:26 -0300

    feat: add read_dataset_dir function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{error::Error, io, process};
+use std::{error::Error, fs, io, process};
+
 extern crate csv;
 extern crate serde;
 #[macro_use]
@@ -18,29 +19,42 @@ struct WFile<'a>{
     country: &'a str,
 }
 
-fn parse(path_r: &str) -> Result<(), Box<dyn Error>> {
-    //TODO: read database folder for .csv files and not receive it as a param
-    let rdr= csv::ReaderBuilder::new()
-    .flexible(true)
-    .from_path(path_r)?;
-    let rdr2= csv::ReaderBuilder::new()
-    .flexible(true)
-    .from_path("dataset/survey_results_public.csv")?;
-    let files = vec![rdr, rdr2];
+fn read_dataset_dir(dir: &str) -> Vec<String> {
+    let paths = fs::read_dir(dir).expect("error: problem to read dataset path");
+    let names =
+    paths.filter_map(|entry| {
+      entry.ok().and_then(|e|
+        e.path().file_name()
+        .and_then(|n| n.to_str().map(|s| String::from(s)))
+      )
+    }).collect::<Vec<String>>();
+    names
+}
+
+fn parse() -> Result<(), Box<dyn Error>> {
+    let files_from_dir = read_dataset_dir("./dataset");
+    let mut files = vec![];
+
+    for file in files_from_dir {
+        let file_name = format!("dataset/{}", file.as_str());
+        files.push(csv::ReaderBuilder::new()
+        .flexible(true)
+        .from_path(file_name).expect("error: problem to create vector of files"));
+    }
+
     let mut wtr = csv::Writer::from_writer(io::stdout());
     for mut file in files {
         for data in file.deserialize() {
             let f: Record = data?; 
-                wtr.serialize( WFile{id: f.respondent, country: f.country.as_str()})?;
+            wtr.serialize( WFile{id: f.respondent, country: f.country.as_str()}).expect("error: problem while serializing");
         } 
-        wtr.flush()?;
+        wtr.flush().expect("error: was not able to write file");
     }
     Ok(())
 }
 
 fn main() {
-    //TODO: finish and make the README greater
-    match parse("dataset/2017.csv") {
+    match parse() {
         Ok(())=> {}
         Err(err) => {
             println!("{}", err);
